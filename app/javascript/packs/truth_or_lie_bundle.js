2c9dbd1bd39b21162f9680697d86dba78da69205
Ollie <ollie.bhm@gmail.com>
2018-03-30 21:50:44 +0100

    Remove unneeded imports

--- app/javascript/packs/truth_or_lie_bundle.js
+++ app/javascript/packs/truth_or_lie_bundle.js
@@ -2,11 +2,6 @@ import ReactOnRails from 'react-on-rails';
 
 import TruthOrLie from '../bundles/TruthOrLie/components/TruthOrLie';
 
-// are these needed to be imported or does the above import them?
-import Vote from '../bundles/TruthOrLie/components/Vote';
-import Result from '../bundles/TruthOrLie/components/Result';
-import ApiService from '../bundles/TruthOrLie/services/ApiService';
-
 // This is how react_on_rails can see the TruthOrLie in the browser.
 ReactOnRails.register({
   TruthOrLie
