d42088e12c956afcd63bd26adaaf2d8319eaa20c
Ollie <ollie.bhm@gmail.com>
2018-03-30 22:06:30 +0100

    Add actioncable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server = "wss://#{ENV['APPLICATION_DOMAIN']}/cable"
+  config.action_cable.allowed_request_origins = ["https://#{ENV['APPLICATION_DOMAIN']}", "http://#{ENV['APPLICATION_DOMAIN']}"]
 end
