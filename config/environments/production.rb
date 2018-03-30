f294ee7b78621546491fd33b4b6da0b66edeee89
Ollie <ollie.bhm@gmail.com>
2018-03-30 22:42:12 +0100

    Add session store key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://#{ENV['APPLICATION_DOMAIN']}/cable"
+  config.action_cable.allowed_request_origins = ["https://#{ENV['APPLICATION_DOMAIN']}", "http://#{ENV['APPLICATION_DOMAIN']}"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -91,7 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.web_socket_server = "wss://#{ENV['APPLICATION_DOMAIN']}/cable"
-  config.action_cable.allowed_request_origins = ["https://#{ENV['APPLICATION_DOMAIN']}", "http://#{ENV['APPLICATION_DOMAIN']}"]
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: '_draz_truth_or_lie_session', :domain => :all, :tld_length => 2
