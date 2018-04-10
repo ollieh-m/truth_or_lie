4a7ef9dda0e9da9618e3c42facd02f393664ce81
Ollie <ollie.bhm@gmail.com>
2018-04-10 23:27:27 +0100

    Delete transition history daily

--- config/clock.rb
+++ config/clock.rb
@@ -6,4 +6,9 @@ module Clockwork
   every(30.seconds, 'RevealAndRestartProposition') do
     RevealAndRestartJob.perform_later
   end
+
+  # temporary fix for storing too much transition history
+  every(1.day, :at => '00:00') do
+    PropositionTransition.where(most_recent: false).destroy_all
+  end
 end
